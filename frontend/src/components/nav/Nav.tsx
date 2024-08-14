8d16d6d507e606933f0d95288097e8bd68002cd0
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-08-14 15:39:30 -0400

    (atlas)edit:Nav(ajuste en lu y pantallas)

--- frontend/src/components/nav/Nav.tsx
+++ frontend/src/components/nav/Nav.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link as LinkRouter } from 'react-router-dom';
 import { Link as LinkScroll } from 'react-scroll';
 import { useState } from "react";
 import { FaTimes } from "react-icons/fa";
@@ -9,54 +8,82 @@ import { VscTerminal } from "react-icons/vsc";
 const Nav: React.FC = () => {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
-  const content: any = <>
+
+  const content = (
     <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition">
       <ul className="text-center text-xl p-20">
-        <LinkScroll spy={true} smooth={true} to="inicio">
-          <li className="my-4 py-4 border-slate-800 hover:bg-slate-800 hover:rounded">Inicio</li>
-        </LinkScroll>
-        <LinkScroll spy={true} smooth={true} to="about">
-          <li className="my-4 py-4 border-slate-800 hover:bg-slate-800 hover:rounded">Sobre mí</li>
-        </LinkScroll>
-        <LinkRouter to="/book">
-          <li className="my-4 py-4 border-slate-800 hover:bg-slate-800 hover:rounded">Lee mi Libro</li>
-        </LinkRouter>
-        <LinkScroll spy={true} smooth={true} to="contact">
-          <li className="my-4 py-4 border-slate-800 hover:bg-slate-800 hover:rounded">Contacto</li>
-        </LinkScroll>
+        <li className="my-4 py-4 border-slate-800 hover:bg-slate-800 hover:rounded">
+          <LinkScroll spy={true} smooth={true} to="Proyectos" onClick={handleClick}>
+            Proyectos
+          </LinkScroll>
+        </li>
+        <li className="my-4 py-4 border-slate-800 hover:bg-slate-800 hover:rounded">
+          <LinkScroll spy={true} smooth={true} to="SobreMi" onClick={handleClick}>
+            Sobre mí
+          </LinkScroll>
+        </li>
+        <li className="my-4 py-4 border-slate-800 hover:bg-slate-800 hover:rounded">
+          <LinkScroll spy={true} smooth={true} to="Skills" onClick={handleClick}>
+            Skills
+          </LinkScroll>
+        </li>
+        <li className="my-4 py-4 border-slate-800 hover:bg-slate-800 hover:rounded">
+          <LinkScroll spy={true} smooth={true} to="Ebook" onClick={handleClick}>
+            Lee mi Libro
+          </LinkScroll>
+        </li>
+        <li className="my-4 py-4 border-slate-800 hover:bg-slate-800 hover:rounded">
+          <LinkScroll spy={true} smooth={true} to="Contact" onClick={handleClick}>
+            Contacto
+          </LinkScroll>
+        </li>
       </ul>
     </div>
-  </>
+  );
 
   return (
     <nav>
-      <div className="h-10vh flex justify-between z-50  bg-slate-900 text-white lg:py-5 px-20 py-4">
+      <div className="absolute top-0 left-0 right-0 h-10vh flex justify-between z-[999] bg-slate-900 text-white lg:py-5 px-20 py-4">
         <div className="flex items-center flex-1">
           <span className="text-3xl font-bold"><VscTerminal /></span>
         </div>
-        <div className="lg:flex md:flex items center justify-end font-normal hidden">
+        <div className="lg:flex items-center justify-end font-normal hidden">
           <div className="flex-10">
             <ul className="flex gap-8 mr-16 text-[18]">
-              <LinkScroll spy={true} smooth={true} to="inicio">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Inicio</li>
-              </LinkScroll>
-              <LinkScroll spy={true} smooth={true} to="about">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Sobre mi</li>
-              </LinkScroll>
-              <LinkRouter to="/book">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Lee mi Libro</li>
-              </LinkRouter>
-              <LinkScroll spy={true} smooth={true} to="/contact">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Contacto</li>
-              </LinkScroll>
+              <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                <LinkScroll spy={true} smooth={true} to="Proyectos">
+                  Proyectos
+                </LinkScroll>
+              </li>
+              <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                <LinkScroll spy={true} smooth={true} to="SobreMi">
+                  Sobre mí
+                </LinkScroll>
+              </li>
+              <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                <LinkScroll spy={true} smooth={true} to="Skills">
+                  Skills
+                </LinkScroll>
+              </li>
+              <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                <LinkScroll spy={true} smooth={true} to="Ebook">
+                  Lee mi Libro
+                </LinkScroll>
+              </li>
+              <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                <LinkScroll spy={true} smooth={true} to="Contact">
+                  Contacto
+                </LinkScroll>
+              </li>
             </ul>
           </div>
         </div>
-        <div>{click && content} </div>
-        <button className="block sm:hidden transition" onClick={handleClick}>{ click ? <FaTimes/> : <CiMenuFries/>}</button>
+        <div>{click && content}</div>
+        <button className="block lg:hidden transition" onClick={handleClick}>
+          {click ? <FaTimes /> : <CiMenuFries />}
+        </button>
       </div>
     </nav>
-
   );
 };
 
